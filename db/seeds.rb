cfc24caccff5ea669e1de553d2f17bc88bf3cd2d
Alan Seto <alantseto@gmail.com>
2016-07-08 12:02:21 -0500

    create dummy objects to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Generate Users
+User.create(email: 'admin@cask.com', password: 'password', password_confirmation: 'password')
+User.create(email: 'user1@cask.com', password: 'password', password_confirmation: 'password')
+User.create(email: 'user2@cask.com', password: 'password', password_confirmation: 'password')
+User.create(email: 'user3@cask.com', password: 'password', password_confirmation: 'password')
+User.create(email: 'user4@cask.com', password: 'password', password_confirmation: 'password')
+
+# Generate Breweries
+Brewery.create(brewery_name: 'brewery0', brewery_country: 'USA', brewery_location: 'Chicago', beer_count: 0, owner_id: 0)
+Brewery.create(brewery_name: 'brewery1', brewery_country: 'USA', brewery_location: 'Chicago', beer_count: 0, owner_id: 1)
+Brewery.create(brewery_name: 'brewery2', brewery_country: 'USA', brewery_location: 'Chicago', beer_count: 0, owner_id: 2)
+Brewery.create(brewery_name: 'brewery3', brewery_country: 'USA', brewery_location: 'Chicago', beer_count: 0, owner_id: 3)
+Brewery.create(brewery_name: 'brewery4', brewery_country: 'USA', brewery_location: 'Chicago', beer_count: 0, owner_id: 4)
