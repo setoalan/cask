433382a716d905a3ec7c6068a994acb7b8fd0289
Alan Seto <alantseto@gmail.com>
2016-07-09 23:59:05 -0500

    add tabs to navbar based on role

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,14 +14,12 @@ User.create(email: 'user4@cask.com', password: 'password', password_confirmation
 User.create(email: 'user5@cask.com', password: 'password', password_confirmation: 'password')
 
 # Generate Breweries
-Brewery.create(brewery_name: 'brewery1', brewery_country: 'USA', brewery_location: 'Chicago', beer_count: 0, user_id: 1)
 Brewery.create(brewery_name: 'brewery2', brewery_country: 'USA', brewery_location: 'Chicago', beer_count: 0, user_id: 2)
 Brewery.create(brewery_name: 'brewery3', brewery_country: 'USA', brewery_location: 'Chicago', beer_count: 0, user_id: 3)
 Brewery.create(brewery_name: 'brewery4', brewery_country: 'USA', brewery_location: 'Chicago', beer_count: 0, user_id: 4)
 Brewery.create(brewery_name: 'brewery5', brewery_country: 'USA', brewery_location: 'Chicago', beer_count: 0, user_id: 5)
 
 # Generate Beers
-Beer.create(beer_name: 'beer1', beer_abv: 100, beer_ibu: 100, beer_description: 'beer1 description', beer_style: 'Ale', brewery_id: 1)
 Beer.create(beer_name: 'beer2', beer_abv: 100, beer_ibu: 100, beer_description: 'beer2 description', beer_style: 'Ale', brewery_id: 2)
 Beer.create(beer_name: 'beer3', beer_abv: 100, beer_ibu: 100, beer_description: 'beer3 description', beer_style: 'Ale', brewery_id: 3)
 Beer.create(beer_name: 'beer4', beer_abv: 100, beer_ibu: 100, beer_description: 'beer4 description', beer_style: 'Ale', brewery_id: 4)
