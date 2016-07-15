4516f771a49fe099ff50300ce69963c43187808d
Alan Seto <alantseto@gmail.com>
2016-07-15 12:02:40 -0500

    minor fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,7 @@
 module UsersHelper
   def validate_user
-    if @user
-      if @user != current_user && current_user.role != 'admin'
-        redirect_to root_path, alert: 'Access denied.'
-      end
+    if @user && @user != current_user && current_user.role != 'admin'
+      redirect_to root_path, alert: 'Access denied.'
     end
   end
 end
