b069daf33ab85b30a9b03ddaae167c21292ee538
Alan Seto <alantseto@gmail.com>
2016-07-16 13:47:58 -0500

    streamline all breweries views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def validate_user
-    if @user != current_user && current_user.role != 'admin'
+    if @user && @user != current_user && current_user.role != 'admin'
       redirect_to root_path, alert: 'Access denied.'
     end
   end
