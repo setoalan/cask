23b68e3c1ac76905967f8b61758b96cc7eee7805
Alan Seto <alantseto@gmail.com>
2016-08-03 12:27:20 -0500

    update route file and delete spec file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-
-  get 'categories/show'
-
   devise_for :users
   resources :users do
     resources :breweries do
@@ -18,6 +14,8 @@ Rails.application.routes.draw do
     put :star, on: :member
   end
   resources :categories
+  get 'categories/index'
+  get 'categories/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
